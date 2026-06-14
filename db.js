b85306630e9bc78dfa3e95c2d84db6a45f62e524
Abdul Rehman <sabdulrehman762@gmail.com>
2026-06-14 16:16:45 +0500

    Fix mongoURL reference error in db.js

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
-const mongoose = require("mongoose");
-require('dotenv').config();
+const mongoose = require('mongoose');
 
-const dbURI = process.env.MONGO_URI || "mongodb+srv://sabdulrehman762_db_user:L3jEMKuI4LBLmL7U@cluster0.clrcj5o.mongodb.net/?appName=Cluster0";
+// Define the variable using Render's environment variable, with your Atlas string as a backup
+const mongoURL = process.env.MONGO_URI || "mongodb+srv://sabdulrehman762_db_user:L3jEMKuI4LBLmL7U@cluster0.clrcj5o.mongodb.net/?appName=Cluster0";
 
 mongoose.connect(mongoURL);
 
-const db = mongoose.connection;
+// ... keep the rest of your connection logic (db.on('connected', etc.)) below this
 
 db.on('connected', () => { console.log("Db is connected") });
 db.on('disconnected', () => { console.log("DB is disconnected") });

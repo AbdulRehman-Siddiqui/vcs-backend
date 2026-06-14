14183d2695098bcacb2abf41216b4d488b2a6c60
Abdul Rehman <sabdulrehman762@gmail.com>
2026-06-14 18:47:41 +0500

    Configure for Vercel deployment

--- server.js
+++ server.js
@@ -36,3 +36,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
+module.exports = app;
